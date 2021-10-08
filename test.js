f7e0a8dd19cf067d219b43f0ba8057a45feb9724
Gustav <56839332+Gustav16@users.noreply.github.com>
2021-10-08 17:20:37 +0200

    qq

--- test.js
+++ test.js
@@ -1,25 +1,57 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
 class Polygon {
-
-
-
-    constructor() {
-
-        this.width = 30;
+    constructor(width, height) {
+        this.width = width;
+        this.height = height
         this.color = "red"
-
-
+        this.x = 100
+        this.y = 4
     }
 
 
     draw() {
-
-
+        //console.log("Test")
         ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.fillRect(100, 100, this.width, this.width);
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
 
     }
 
 
 
 }
+
+
+
+
+
+
+class Square extends Polygon {
+    constructor(width, height) {
+        
+        super();
+        this.width = width;
+        this.height = height
+
+
+        // this.y=y
+        this.width = 100
+        this.x = 400
+    }
+
+
+
+}
+
+
+var figur1 = new Polygon(10, 20);
+
+
+
+
+var figur2 = new Square(100, 12);
+
+figur1.draw();
+figur2.draw();
