3125ab306bd3c967b44fda0b8edefa850bc1cf38
Gustav <gustav@lundstroem.dk>
2021-10-17 14:35:05 +0200

    e

--- test.js
+++ test.js
@@ -11,19 +11,24 @@ const ctx = canvas.getContext('2d')
 
 ctx.beginPath();
 ctx.fillStyle = "green";
-ctx.fillRect(0,0,1000,1000)
+ctx.fillRect(0, 0, 1000, 1000)
 
 
 
 
 
 
-/*
-var count = 0
 
-function dummy() {
 
-    console.log(count)
+
+class Dummy {
+    constructor() {
+
+        this.su = 100;
+
+
+    }
+
 
 
 
@@ -31,144 +36,15 @@ function dummy() {
 
 
 
+console.log(Dummy.su)
 
 
+var dummy = new Dummy();
 
+console.log(dummy.su)
 
-function intaval(functionName, repetition, intaval, time) {
 
-    //console.log("test")
 
-    rep();
 
 
-    function rep() {
-
-
-
-        functionName;
-        //dummy();
-
-
-
-        count++;
-
-
-
-
-
-
-
-        if (count < repetition) {
-
-
-
-            setTimeout(rep, 300);
-
-
-        }
-    }
-
-
-
-}
-
-
-intaval( dummy(), 3);
-
-
-
-
-
-function makeEnemie() {
-    enemies[counter] = new Enemy(map[3][0].x, map[3][0].y, counter)
-    counter++;
-
-    if (counter < 10) {
-
-        setTimeout(makeEnemie, 300);
-    }
-
-}
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-class Polygon {
-    constructor(width, height) {
-        this.width = width;
-        this.height = height
-        this.color = "red"
-        this.x = 100
-        this.y = 4
-    }
-
-
-    draw() {
-        //console.log("Test")
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-
-    }
-
-
-
-}
-
-
-class Square extends Polygon {
-
-    constructor(){
-        super();
-
-
-
-    }
-
-
-    color = "green"
-
-   // var hvem=13
-
-
-
-
-    print(){
-
-
-        console.log(col)
-    }
-
-
-}
-
-
-
-var figur1 = new Polygon(10, 20);
-
-
-
-
-var figur2 = new Square(100, 12);
-
-figur1.draw();
-
-figur2.draw();
-
-figur2.print();
-
-*/
+console.log(Dummy.name)
