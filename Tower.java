6ad873f7dfcca2ae3076185d0720fb2af1af3108
Gustav <56839332+Gustav16@users.noreply.github.com>
2022-03-04 12:42:45 +0100

    working on tower class

--- Tower.java
+++ Tower.java
@@ -1,8 +1,14 @@
 public class Tower {
 
-    Tower(){
-
-
-    }
+    int x, y, timer, listOfTargets, price, NumberOftargets, animationTime;
     
+    int damage, range, attackSpeed;
+    String description;
+    boolean showRange = false;
+
+    Tower(int xPos, int yPos) {
+        this.x = xPos;
+        this.y = yPos;
+    }
+
 }
